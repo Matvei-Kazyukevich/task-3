f72b23e398533cb62e7dcdf6ce5b25375a4fe434
Matvei Kazyukevich <matvei.kazyukevich@devteam.inc>
2022-11-05 16:22:44 +0300

    Orders.hs

--- src/Services/Orders.hs
+++ src/Services/Orders.hs
@@ -1,24 +1,10 @@
 module Services.Orders (getOrders, getOrder) where
 
-import Data.Models (OrderModel(..), ProductModel(..))
-import Data.Entities (Order(..), Product(..), productID)
-import Services.Products (fromProductToProductModel) 
+import Data.Models (OrderModel(..))
+import Services.Base (fromOrderToOrderModel)
+import Data.Entities (productID)
 import qualified Repositories.Orders as OR
 import qualified Repositories.Products as PR
-import Prelude hiding (product)
-
-fromOrderToOrderModel :: Order -> Maybe [Product] -> OrderModel
-fromOrderToOrderModel order maybeProduct = 
-    OrderModel {
-        orderModelID = orderID order,
-        orderModelNumber = orderNumber order,
-        orderProductModel = orderProducts
-    } where
-        orderProducts :: Maybe [ProductModel]
-        orderProducts = 
-            case maybeProduct of 
-                Nothing -> Nothing
-                Just products -> Just $ map (`fromProductToProductModel` Nothing) products
 
 getOrders :: [OrderModel] 
 getOrders = map (`fromOrderToOrderModel` Nothing) OR.getOrders

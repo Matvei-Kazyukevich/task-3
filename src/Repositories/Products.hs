724b0fa1dd13ff6c9ac4a6267115d9eee9040c84
Matvei Kazyukevich <matvei.kazyukevich@devteam.inc>
2022-11-05 16:57:16 +0300

    Products.hs

--- src/Repositories/Products.hs
+++ src/Repositories/Products.hs
@@ -1,6 +1,6 @@
-module Repositories.Products (getProducts, getProduct, getOrderProducts) where
+module Repositories.Products (getProducts, getProduct) where
 
-import Data.Entities (Product(..), OrderProduct(..), productID)
+import Data.Entities (Product(..), productID)
 import Data.Base (Color(..))
 import Lib (safeHead)
 
@@ -14,46 +14,3 @@ getProducts = [
 
 getProduct :: Int -> Maybe Product
 getProduct pid = safeHead $ filter (\x -> productID x == pid) getProducts
-
-getOrderProducts :: [OrderProduct]
-getOrderProducts = [
-        OrderProduct {
-                orderProductID = 1,
-                ordProdOrderID = 1,
-                ordProdProductID = 1
-        },
-        OrderProduct {
-                orderProductID = 2,
-                ordProdOrderID = 1,
-                ordProdProductID = 2
-        },
-        OrderProduct {
-                orderProductID = 3,
-                ordProdOrderID = 2,
-                ordProdProductID = 3
-        },
-        OrderProduct {
-                orderProductID = 4,
-                ordProdOrderID = 3,
-                ordProdProductID = 5
-        },
-        OrderProduct {
-                orderProductID = 5,
-                ordProdOrderID = 4,
-                ordProdProductID = 1
-        },
-        OrderProduct {
-                orderProductID = 6,
-                ordProdOrderID = 4,
-                ordProdProductID = 4
-        }, 
-        OrderProduct {
-                orderProductID = 7,
-                ordProdOrderID = 5,
-                ordProdProductID = 5
-        },
-        OrderProduct {
-                orderProductID = 8,
-                ordProdOrderID = 5,
-                ordProdProductID = 3
-        }]

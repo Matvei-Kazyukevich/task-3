83128e1a57ec385ddb6dc5b6655bfd5e3238043d
Matvei Kazyukevich <matvei.kazyukevich@devteam.inc>
2022-11-05 16:22:07 +0300

    Shops.hs

--- src/Services/Shops.hs
+++ src/Services/Shops.hs
@@ -1,25 +1,10 @@
 module Services.Shops (getShops, getShop) where
 
-import Data.Entities (Shop(..), Product(..), productShopID)
-import Data.Models (ShopModel(..), ProductModel(..))
+import Data.Models (ShopModel(..))
+import Services.Base (fromShopToShopModel)
+import Data.Entities (productShopID)
 import qualified Repositories.Shops as SR
 import qualified Repositories.Products as PR
-import Services.Products ( fromProductToProductModel )
-
-fromShopToShopModel :: Shop -> Maybe [Product] -> ShopModel
-fromShopToShopModel shop maybeProduct = 
-    ShopModel {
-        shopModelID = shopID shop,
-        shopModelName = shopName shop,
-        shopModelAddress = shopAddress shop,
-        shopProductModel = shopProduct
-    } 
-    where
-        shopProduct :: Maybe [ProductModel]
-        shopProduct = 
-            case maybeProduct of
-                Nothing -> Nothing
-                Just products -> Just $ map (`fromProductToProductModel` Nothing) products
 
 getShops :: [ShopModel]
 getShops = map (`fromShopToShopModel` Nothing) SR.getShops

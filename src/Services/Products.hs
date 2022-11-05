22b4819495627660ba2cc9f2a96cc61cb049014f
Matvei Kazyukevich <matvei.kazyukevich@devteam.inc>
2022-11-05 16:21:41 +0300

    Products.hs

--- src/Services/Products.hs
+++ src/Services/Products.hs
@@ -1,41 +1,11 @@
-module Services.Products (getProducts, getProduct, fromProductToProductModel) where
+module Services.Products (getProducts, getProduct) where
 
-import Data.Entities (Product(..), Shop(..), productID, productName, productPrice, productColor)
-import Data.Models (ProductModel(..), ShopModel(..))
+import Data.Models (ProductModel(..))
+import Services.Base (fromProductToProductModel) 
+import Data.Entities (shopProductID)
+import Prelude hiding (product)
 import qualified Repositories.Products as PR
 import qualified Repositories.Shops as SR
-import Prelude hiding (product)
-
-
-fromProductToProductModel :: Product -> Maybe Shop -> ProductModel
-fromProductToProductModel product maybeShop =
-    let 
-        pid = productID product
-        name = productName product
-        price = productPrice product
-        color = productColor product
-    in ProductModel pid name price color productShop 
-    where 
-        productShop :: Maybe ShopModel 
-        productShop = 
-            case maybeShop of
-                Nothing -> Nothing
-                Just shop -> Just $ fromShopToShopModel shop Nothing
-
-fromShopToShopModel :: Shop -> Maybe [Product] -> ShopModel
-fromShopToShopModel shop maybeProduct =
-    ShopModel {
-        shopModelID = shopID shop,
-        shopModelName = shopName shop,
-        shopModelAddress = shopAddress shop,
-        shopProductModel = shopProduct
-    } where
-        shopProduct :: Maybe [ProductModel]
-        shopProduct = 
-            case maybeProduct of
-                Nothing -> Nothing
-                Just products -> Just $ map (`fromProductToProductModel` Nothing) products
-
 
 getProducts :: [ProductModel]
 getProducts = map (`fromProductToProductModel` Nothing) PR.getProducts 

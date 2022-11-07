0f7126bef73cc8ca3e4f947122430b475040bc68
Matvei Kazyukevich <matvei.kazyukevich@devteam.inc>
2022-11-07 14:24:08 +0300

    Entities.hs

--- src/Data/Entities.hs
+++ src/Data/Entities.hs
@@ -1,30 +1,34 @@
-module Data.Entities (Product(..), Shop(..), Order(..), Customer(..), productID, productShopID, productName, productPrice, productColor) where
+module Data.Entities (Product(..), Shop(..), Order(..), Customer(..), productID, productShopID, productOrderID, productName, productPrice, productColor) where
 
 import Data.Base (Color(..))
 
 type ProductID = Int
 type ProductShopID = Int
+type ProductOrderID = Int
 type ProductName = String
 type ProductPrice = Double
 type ProductColor = Color
 
-data Product = Product ProductID ProductShopID ProductName ProductPrice ProductColor
+data Product = Product ProductID ProductShopID ProductOrderID ProductName ProductPrice ProductColor
     deriving (Show)
 
 productID :: Product -> ProductID
-productID (Product pid _ _ _ _) = pid
+productID (Product pid _ _ _ _ _) = pid
 
 productShopID :: Product -> ProductShopID
-productShopID (Product _ psid _ _ _) = psid
+productShopID (Product _ psid _ _ _ _) = psid
+
+productOrderID :: Product -> ProductOrderID
+productOrderID (Product _ _ poid _ _ _) = poid
 
 productName :: Product -> ProductName
-productName (Product _ _ name _ _) = name
+productName (Product _ _ _ name _ _) = name
 
 productPrice :: Product -> ProductPrice
-productPrice (Product _ _ _ price _) = price
+productPrice (Product _ _ _ _ price _) = price
 
 productColor :: Product -> ProductColor
-productColor (Product _ _ _ _ color) = color
+productColor (Product _ _ _ _ _ color) = color
 
 data Shop = Shop {
       shopID :: Int
@@ -44,3 +48,9 @@ data Order = Order {
     , orderCustomerID :: Int
     , orderNumber :: String 
 } deriving (Show)
+
+-- data OrderProduct = OrderProduct {
+--     orderProductID :: Int
+--   , ordProdOrderID :: Int
+--   , ordProdProductID :: Int
+-- } deriving (Show)

43443f12522e17cbc6c615dedb7f1142346a3c02
Matvei Kazyukevich <matvei.kazyukevich@devteam.inc>
2022-11-07 14:24:49 +0300

    Products.hs

--- src/Services/Orders.hs
+++ src/Services/Orders.hs
@@ -2,7 +2,7 @@ module Services.Orders (getOrders, getOrder) where
 
 import Data.Models (OrderModel(..))
 import Services.Base (fromOrderToOrderModel)
-import Data.Entities (productID)
+import Data.Entities (productOrderID)
 import qualified Repositories.Orders as OR
 import qualified Repositories.Products as PR
 
@@ -16,4 +16,4 @@ getOrder oid =
         Nothing -> Nothing
         Just order -> Just $ fromOrderToOrderModel order products
             where 
-                products = Just $ filter (\x -> productID x == oid) PR.getProducts
+                products = Just $ filter (\x -> productOrderID x == oid) PR.getProducts
